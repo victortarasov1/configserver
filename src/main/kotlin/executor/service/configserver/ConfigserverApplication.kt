1ab3ce58a5d207a3c89c2f2f5f8eef454ccbf6ae
victortarasov1 <victortarasov13032001@gmail.com>
2024-06-21 03:58:14 -0700

    updated

--- src/main/kotlin/executor/service/configserver/ConfigserverApplication.kt
+++ src/main/kotlin/executor/service/configserver/ConfigserverApplication.kt
@@ -2,8 +2,10 @@ package executor.service.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class ConfigserverApplication
 
 fun main(args: Array<String>) {
